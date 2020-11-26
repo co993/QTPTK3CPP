a2433de02b65abc24c12d31a092a6b14f95abe11
DESKTOP-7DIND06\UPC <kophyogyi701@gmail.com>
2020-11-26 14:14:42 +0630

    FirstCommit2

--- renderarea.cpp
+++ renderarea.cpp
@@ -1,14 +1,15 @@
 #include "renderarea.h"
 #include <QPaintEvent>
 #include <QPainter>
-
+#include <math.h>
 
 RenderArea::RenderArea(QWidget *parent) :
     QWidget(parent),
     mBackgroundColor (0, 0, 255),
-    mShapeColor (255, 150, 255)
-
+    mShapeColor (255, 150, 255),
+    mShape(Astroid)
 {
+    on_shape_changed();
 }
 QSize RenderArea:: minimumSizeHint () const
 {
@@ -19,6 +20,80 @@ QSize RenderArea:: sizeHint () const
 {
     return QSize(400, 200);
 }
+void RenderArea::on_shape_changed (){
+    switch(mShape){
+    case Astroid:
+    mScale=40;
+    mintervalLength =2* M_PI;
+    mStepCount=256;
+
+  case Cycloid:
+    break;
+
+
+    case HuygensCycloid:
+
+        break;
+    case HypoCycloid:
+
+
+
+        break;
+    case FutureCurves:
+
+        break;
+      default:
+        break;
+    }
+}
+QPointF RenderArea::compute(float t){
+    switch(mShape){
+    case Astroid:
+return compute_astroid(t);
+
+
+    break;
+   case Cycloid:
+return compute_cycloid(t);
+      break;
+    case HuygensCycloid:
+return compute_huygens_cycloid(t);
+        break;
+    case HypoCycloid:
+  return compute_hypo_cycloid(t);
+        break;
+    case FutureCurves:
+return compute_future_curves(t);
+        break;
+      default:
+        break;
+    }
+}
+QPointF RenderArea::compute_astroid(float t){
+ Q_UNUSED(t);
+ float cos_t =cos (t);
+ float sin_t =sin (t);
+ float x=2 * cos_t* cos_t* cos_t;//pow (cos_t,3);
+ float y=2*  sin_t* sin_t* sin_t;//pow (sin_t,3);
+ return QPointF (x,y);
+}
+QPointF RenderArea::compute_cycloid(float t){
+    Q_UNUSED(t);
+ //TBD
+}
+QPointF RenderArea::compute_huygens_cycloid(float t){
+    Q_UNUSED(t);
+ //TDB
+}
+QPointF RenderArea::compute_hypo_cycloid(float t){
+    Q_UNUSED(t);
+//TBD
+}
+
+QPointF RenderArea::compute_future_curves(float t){
+    Q_UNUSED(t);
+    //TBD:
+}
 void  RenderArea::paintEvent (QPaintEvent *event)
 {
     Q_UNUSED(event);
@@ -26,24 +101,20 @@ void  RenderArea::paintEvent (QPaintEvent *event)
 
  painter.setRenderHint( QPainter::Antialiasing, true);
 
- switch (mShape) {
- case Astroid:mBackgroundColor=Qt::red;
- break;
-    case Cycloid:mBackgroundColor=Qt::green;
-     break;
- case HuygensCycloid:mBackgroundColor=Qt::blue;
-     break;
- case HypoCycloid:mBackgroundColor=Qt::yellow;
-     break;
-   default:
-     break;
- }
+
   painter.setBrush(mBackgroundColor);
    painter.setPen(mShapeColor);
+   painter.drawRect(this->rect());
+   QPoint center=this->rect().center();
  //draw area
- painter.drawRect(this->rect());
- painter.drawLine(this->rect().topLeft(), this->rect().bottomRight());
 
+  float step=mintervalLength/mStepCount;
+for(float t=0;t<mintervalLength;t+=step){
+   QPointF point=compute(t);
+   QPoint pixel;
+   pixel.setX(point.x() *mScale+center.x());
+   pixel.setY(point.y() *mScale+center.y());
+   painter.drawPoint(pixel);
 
-
+}
 }
--- renderarea.h
+++ renderarea.h
@@ -13,24 +13,34 @@ public:
     QSize minimumSizeHint () const Q_DECL_OVERRIDE;
     QSize sizeHint () const Q_DECL_OVERRIDE;
 
-    enum Shapes { Astroid, Cycloid, HuygensCycloid, HypoCycloid };
+    enum Shapes { Astroid, Cycloid, HuygensCycloid, HypoCycloid,FutureCurves};
     void setBackgroundColor (QColor color){mBackgroundColor=color;}//setter
     QColor backgroundColor ()const {return mBackgroundColor;}//getter
-    void setShape (Shapes shape){mShape=shape;}//setter
-    Shapes shape () const {return mShape;}
+    void setShape (Shapes shape){mShape=shape; on_shape_changed();}//setter
+    Shapes shape () const {return mShape;}//getter
 protected:
     void paintEvent (QPaintEvent *event) Q_DECL_OVERRIDE;
 
 signals:
 
 public slots:
-
+private:
+    void on_shape_changed();
+    QPointF compute (float t);
+    QPointF compute_astroid(float t);
+    QPointF compute_cycloid(float t);
+    QPointF compute_huygens_cycloid(float t);
+    QPointF compute_hypo_cycloid(float t);
+    QPointF compute_future_curves(float t);
 private:
     QColor mBackgroundColor;
     QColor mShapeColor;
     Shapes mShape;
 
 
+float mintervalLength;
+float mScale;
+int mStepCount;
 
 
 };
